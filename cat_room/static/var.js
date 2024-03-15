6fa16edb02e03a6216dd64188beb5d706ca16d7a
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2024-03-15 14:21:01 +0100

    stuff :p (username pop-up)

--- cat_room/static/var.js
+++ cat_room/static/var.js
@@ -1,2 +1,4 @@
 const content = document.getElementById("content")
 const inp = document.getElementById("inp");
+const popUp = document.getElementById("pop-up")
+const username_input = document.getElementById("username-input")
