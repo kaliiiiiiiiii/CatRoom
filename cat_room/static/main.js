9bc8305622c0de9514f52925934f1a26bd65a902
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2024-03-15 13:37:14 +0100

    yeeeeeeeeeeeeeeeee

--- cat_room/static/main.js
+++ cat_room/static/main.js
@@ -1,5 +1,10 @@
 function onMessage(user, timeStamp, message, id){
-    // visualize message here @micha
+    let span = document.createElement("span")
+    span.setAttribute("id", id)
+    span.setAttribute("class", "message")
+    span.innerHTML = '<h4 class="username">' + user + '</h4><span class="message-content">' + message + '</span><span class="time-stamp">' + timeStamp + '</span>'
+
+    content.append(span)
     console.log(user, timeStamp, message, id)
 };
 
@@ -14,6 +19,10 @@ function onLeave(user, timeStamp){
 async function main(){
     const con = new Connection(onJoin, onMessage, onLeave)
     await con.register("Cat-2235")
+    try{
+        await con.register("Cat-2235")
+    }catch(e){console.error(e)}
+
     await con.sendMessage("Hello?")
 };
 main()
