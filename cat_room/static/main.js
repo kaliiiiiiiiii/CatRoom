9a29ba555678c555123974c657a93d1cf893b932
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2024-03-16 11:20:44 +0100

    Error duplicate username + some neater css for spans (no more h4 for usernames)

--- cat_room/static/main.js
+++ cat_room/static/main.js
@@ -3,7 +3,7 @@ function onMessage(user, timeStamp, message, id){
     span.setAttribute("id", id)
     span.setAttribute("class", "message")
 
-    let userNameElem = document.createElement("h4")
+    let userNameElem = document.createElement("span")
     userNameElem.setAttribute("class", "username")
     userNameElem.textContent = user
     span.append(userNameElem)
@@ -31,7 +31,7 @@ function onJoin(user, timeStamp){
 
     let spanPrefix = document.createElement("span")
     spanPrefix.setAttribute("class", "user-join-affix")
-    spanPrefix.innerHTML = ">> &nbsp"
+    spanPrefix.innerHTML = "> &nbsp;"
     span.append(spanPrefix)
 
     let userNameElem = document.createElement("span")
@@ -54,7 +54,30 @@ function onJoin(user, timeStamp){
 };
 
 function onLeave(user, timeStamp){
-    console.log(user)
+    let span = document.createElement("span")
+    span.setAttribute("class", "join")
+
+    let spanPrefix = document.createElement("span")
+    spanPrefix.setAttribute("class", "user-leave-affix")
+    spanPrefix.innerHTML = "> &nbsp;"
+    span.append(spanPrefix)
+
+    let userNameElem = document.createElement("span")
+    userNameElem.setAttribute("class", "user-leave")
+    userNameElem.textContent = user
+    span.append(userNameElem)
+
+    let spanAffix = document.createElement("span")
+    spanAffix.setAttribute("class", "user-leave-affix")
+    spanAffix.innerHTML = " left."
+    span.append(spanAffix)
+
+    let timeElem = document.createElement("span")
+    timeElem.setAttribute("class","time-stamp")
+    timeElem.textContent = timeStamp.toLocaleString()
+    span.append(timeElem)
+
+    content.append(span)
 };
 
 async function main(){
@@ -72,6 +95,7 @@ async function main(){
 
     username_input.addEventListener("keydown", (event) => {
         if (event.key !== 'Enter') {
+            warn.style.display = "none";
             return;
           };
         let username = username_input.value;
@@ -79,11 +103,12 @@ async function main(){
         con.register(username)
             .then(()=>{
                 popUp.style.display = 'none';
+                blur.style.display = 'none';
                 bar_username.textContent = username
             })
             .catch((e)=>{
                 if(e.message == "Duplicate User!"){
-                 // @Micha add user duplicate warning (html/css)
+                    warn.style.display = "block";
                 }else{console.error(e)}
             })
         console.log(username)
--- cat_room/static/var.js
+++ cat_room/static/var.js
@@ -3,3 +3,5 @@ const inp = document.getElementById("inp");
 const popUp = document.getElementById("pop-up")
 const username_input = document.getElementById("username-input")
 const bar_username = document.getElementById("bar_username")
+const blur = document.getElementById("blur")
+const warn = document.getElementById("warning")
